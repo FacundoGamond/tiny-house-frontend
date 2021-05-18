c3f7091f3f0537515f1d6ad3cc7a62002205e20c
Facu <facugamond@hotmail.com>
2021-05-17 23:14:22 -0300

    first settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PreHeaderComponent } from './components/pre-header/pre-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PreHeaderComponent
   ],
   imports: [
     BrowserModule
