1ca17f1de606ea74a95b6955972bcefcd8574006
Facu <facugamond@hotmail.com>
2021-05-23 00:04:52 -0300

    hero and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PreHeaderComponent } from './components/pre-header/pre-header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HouseComponent } from './components/house/house.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    PreHeaderComponent
+    PreHeaderComponent,
+    HeroComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public toggler: any;
+  public menu: any;
+  public navbar: any;
+  public logo: any;
+  public prehead: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    //Getting dom elements
+    this.navbar = document.querySelector<HTMLElement>('.navbar');
+    this.toggler = this.navbar.querySelector('[toggler-menu]');
+    this.menu = this.navbar.querySelector('.menu');
+    this.logo = this.navbar.querySelector('.logo');
+    this.onScrollNavbar();
+  }
+
+  openMenu() {
+    const brandingHeight = document.querySelector('.c-navbar').clientHeight;
+
+    this.menu.style.top = brandingHeight + 'px';
+
+    if (this.toggler.getAttribute('aria-expanded') == 'false') {
+
+      this.menu.setAttribute('aria-expanded', 'true');
+
+      this.toggler.setAttribute('aria-expanded', 'true')
+
+    } else {
+
+      this.menu.setAttribute('aria-expanded', 'false');
+
+      this.toggler.setAttribute('aria-expanded', 'false');
+
+    }
+
+  }
+
+  onScrollNavbar(){
+    window.addEventListener('scroll', ()=>{
+      if(window.scrollY > 74){
+        this.toggler.classList.add('on-scroll');
+        this.navbar.classList.add('on-scroll');
+        this.logo.classList.add('on-scroll');
+      }else{
+        this.toggler.classList.remove('on-scroll');
+        this.navbar.classList.remove('on-scroll');
+        this.logo.classList.remove('on-scroll');
+      }
+    });
   }
 
 }
