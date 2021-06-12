2b31db25bf155963fb46dc770623ba37e866fff1
Facu <facugamond@hotmail.com>
2021-06-12 17:51:52 -0300

    form ready

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -12,4 +12,57 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  submitForm() {
+    event.preventDefault();
+    const name =  (<HTMLInputElement>document.querySelector('#name')).value;
+    const phone =  (<HTMLInputElement>document.querySelector('#phone')).value;
+    const email =  (<HTMLInputElement>document.querySelector('#email')).value;
+    const place =  (<HTMLInputElement>document.querySelector('#place')).value;
+
+    const clearForm = () => {
+      (<HTMLInputElement>document.querySelector('#name')).value = '';
+      (<HTMLInputElement>document.querySelector('#phone')).value = '';
+      (<HTMLInputElement>document.querySelector('#email')).value = '';
+      (<HTMLInputElement>document.querySelector('#place')).value = '';
+    }
+
+    const sendMessage = message => {
+      const modal = document.querySelector('.c-contact__modal');
+      modal.innerHTML = message;
+      modal.classList.add('show');
+
+      setTimeout(() => {
+        modal.classList.remove('show');
+      }, 5000);
+    }
+
+    if(name && phone && email && place){
+
+      $.ajax({
+        type: 'POST',
+        url: "http://localhost:7000/api/send-email",
+        data: {
+          name,
+          phone,
+          email,
+          place
+        }, //que envio
+        beforeSend: function () {
+          sendMessage('Enviando mensaje.')
+        },
+        success: function (response) {
+          clearForm();
+          sendMessage('Mensaje enviado!')
+        },
+        error: function () {
+          sendMessage('Ups! hubo un error, intentalo más tarde.')
+        },
+        timeout: 10000
+      })
+    }else{
+      sendMessage('Error, rellena todos los campos!')
+    }
+
+  }
 }
